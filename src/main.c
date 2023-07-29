0c936e3d36f7b3285277adf4e72c531cfd0c94ea
Simon Zweers <s.zweers21@gmail.com>
2023-07-30 00:32:32 +0200

    Scan of current directory

--- src/main.c
+++ src/main.c
@@ -3,4 +3,15 @@
 
 int main(int argc, char **argv) {
     printf("Welcom to ls C, an alternative to LS\n");
+
+    // Example from https://www.sanfoundry.com/c-program-list-files-directory/
+    DIR *dir;
+    struct dirent *dir_entry;
+    dir = opendir(".");
+    if (dir) {
+        while (readdir(dir) != NULL) {
+            dir_entry = readdir(dir);
+            printf("%s\n", dir_entry->d_name);
+        }
+    }
 }
