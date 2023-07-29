d796f79782a8157a5f411a69802b231066878c8a
Simon Zweers <s.zweers21@gmail.com>
2023-07-30 00:25:52 +0200

    Initial commit

---
+++ include/lsc.h
@@ -0,0 +1,8 @@
+//
+// Created by on 7/30/23.
+//
+
+#ifndef LSC_LSC_H
+#define LSC_LSC_H
+
+#endif //LSC_LSC_H
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char **argv) {
+    printf("Welcom to ls C, an alternative to LS\n");
+}
