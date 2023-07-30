29bb13b670b478681d4a8d9a32fd6ca91c3525b0
Simon Zweers <s.zweers21@gmail.com>
2023-07-31 00:25:52 +0200

    Moved code to function

--- src/main.c
+++ src/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main(int argc, char **argv) {
-    printf("Welcom to ls C, an alternative to LS\n");
-
+void printDir(const char *dir_to_open) {
     // Example from https://www.sanfoundry.com/c-program-list-files-directory/
     DIR *dir;
     struct dirent *dir_entry;
-    dir = opendir(".");
+    dir = opendir(dir_to_open);
     if (dir) {
         while (readdir(dir) != NULL) {
             dir_entry = readdir(dir);
             printf("%s\n", dir_entry->d_name);
         }
     }
+}
+
+
+int main(int argc, char **argv) {
+    printf("Welcome to ls C, an alternative to LS\n");
+    printDir(".");
 }
